0513732e5523c7aba6db27eb4e5bfb17c4d0a1f9
Xavi <ensable@gmail.com>
2025-08-17 16:33:16 +0200

    chore: adding storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/react-native-web-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/react-native-web-vite",
+    "options": {}
+  }
+};
+export default config;
